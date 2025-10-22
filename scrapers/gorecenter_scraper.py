bd77bceccfb2d9cd9855518303fe7296e7ffc9ea
nscerr <NatSpencer699@proton.me>
2025-10-22 23:34:09 +0700

    Create gorecenter_scraper.py

---
+++ scrapers/gorecenter_scraper.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+def scrape(url_postingan):
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+        response = requests.get(url_postingan, headers=headers)
+        response.raise_for_status() 
+        html_konten = response.text
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Gagal mengambil HTML: {e}"}
+
+    soup = BeautifulSoup(html_konten, 'html.parser')
+    
+    judul_artikel = "Judul Tidak Ditemukan"
+    isi_artikel = "Isi Artikel Tidak Ditemukan"
+    list_url_video = [] 
+
+    judul_element = soup.find('h1', class_='entry-title')
+    if judul_element:
+        judul_artikel = judul_element.text.strip()
+        
+    content_container = soup.find('div', class_='entry-content')
+    
+    if content_container:
+        semua_source_tags = content_container.find_all('source')
+        if semua_source_tags:
+            for source_tag in semua_source_tags:
+                if source_tag.has_attr('src'):
+                    video_src = source_tag['src']
+                    list_url_video.append(urljoin(url_postingan, video_src))
+        
+        paragraf_list = content_container.find_all('p')
+        if paragraf_list:
+            isi_artikel = "\n\n".join([p.text.strip() for p in paragraf_list])
+            
+    if not list_url_video:
+        list_url_video.append("URL Video Tidak Ditemukan")
+
+    return {
+        "situs": "gorecenter",
+        "judul": judul_artikel,
+        "video_urls": list_url_video,
+        "isi_artikel": isi_artikel
+    }
